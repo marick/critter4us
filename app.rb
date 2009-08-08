c0fcd3f8fa05934e59e373c007c9ed966a75d0ab
Brian Marick <marick@exampler.com>
2009-08-08 10:58:30 -0500

    removed actual persistent store files

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require 'rubygems'
 require File.expand_path('path-setting', File.dirname(__FILE__))
 
 require 'config'
-require 'persistent-store'
 require 'controller'
 
 Controller.run! :host => 'localhost', :port => 7000
