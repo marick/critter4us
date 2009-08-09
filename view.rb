d0a752d1d3bcf7fd576dbc784e5a708e9f50a853
Brian Marick <marick@exampler.com>
2009-08-08 20:30:15 -0500

    a simple view for completed reservations

---
+++ view.rb
@@ -0,0 +1,33 @@
+require 'erector'
+
+class ReservationDescription < Erector::Widget
+
+  def name_list(names)
+    ul do
+      names.each do | name | 
+        li { text name }
+      end
+    end
+  end
+
+  def content
+    html do 
+      head do
+        title "Reservation #{@reservation.id}"
+      end
+      body do
+        p do
+          text "Reservation for #{@reservation.date}"
+        end
+        p do
+          text "These animals are reserved:"
+          name_list(@reservation.animal_names)
+        end
+        p do 
+          text "These procedures will be done:"
+          name_list(@reservation.procedure_names)
+        end
+      end
+    end
+  end
+end
