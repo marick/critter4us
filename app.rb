fb5591cf97a224c9d341a204087eaf85e116a77d
Brian Marick <marick@exampler.com>
2009-08-14 10:49:29 -0500

    listen on different ports when in production

--- app.rb
+++ app.rb
@@ -4,4 +4,10 @@ require File.expand_path('path-setting', File.dirname(__FILE__))
 require 'config'
 require 'controller'
 
-Controller.run! :host => 'localhost', :port => 7000
+Sinatra::Base.configure :development do
+  Controller.run! :host => 'localhost', :port => 7000
+end
+
+Sinatra::Base.configure :production do 
+  Controller.run!
+end
