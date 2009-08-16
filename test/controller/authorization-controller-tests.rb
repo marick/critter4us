3e01e15864d2889a6267d62f19554024c526abea
Brian Marick <marick@exampler.com>
2009-08-16 16:29:58 -0500

    subdirectories

---
+++ test/controller/authorization-controller-tests.rb
@@ -0,0 +1,32 @@
+$: << '../..' unless $in_rake
+require 'test/testutil/requires'
+require 'controller'
+
+class AuthorizationControllerTests < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  attr_reader :app
+
+  def setup
+    empty_tables
+    @app = Controller.new
+    @dummy_view = TestViewClass.new
+  end
+
+  context "viewing reservations" do
+    should "pass a list of reservations to the view" do
+      DB.populate do
+        @expected_reservation = Reservation.random do
+          use(@expected_animal = Animal.random)
+          use(@expected_procedure = Procedure.random)
+        end
+      end
+
+      @app.test_view_builder = @dummy_view
+      get '/reservations'
+      assert { @dummy_view[:reservations].size == 1 }
+      actual_reservation = @dummy_view[:reservations].first
+      assert { actual_reservation == @expected_reservation }
+    end
+  end
+end
