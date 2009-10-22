38e9e647f6672d96734d446fa58b45574ef6b69a
Brian Marick <marick@exampler.com>
2009-10-22 08:28:28 -0500

    empty version

---
+++ test/model/procedure-rules-tests.rb
@@ -0,0 +1,11 @@
+$: << '../..' unless $in_rake
+require 'test/testutil/requires'
+require 'model/requires'
+
+class ProcedureRulesTests < FreshDatabaseTestCase
+
+  should_eventually "test add)-excluded_pairs" do
+  end
+
+end
+
