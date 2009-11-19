7aac0d45002129a40a7dce56c9f530fe36baa354
Brian Marick <marick@exampler.com>
2009-11-19 16:20:25 -0500

    The animal list view generates delete buttons that pass along effective dates.

--- test/view/animal-list-view-tests.rb
+++ test/view/animal-list-view-tests.rb
@@ -12,7 +12,9 @@ class AnimalViewTests < FreshDatabaseTestCase
     Animal.random(:name => '333')
     Animal.random(:name => '111')
     Animal.random(:name => '222')
-    @view = AnimalListView.new(:animals => Animal.all)
+    @date_source = flexmock("date source",
+                            :current_date_as_string => '2010-09-08').by_default
+    @view = AnimalListView.new(:animals => Animal.all, :date_source => @date_source)
   end
   
   should "sort animals by name" do
@@ -29,8 +31,12 @@ class AnimalViewTests < FreshDatabaseTestCase
   end
 
   should "contain a delete button" do
-    html = @view.to_s
-    assert { html.include? delete_button("animal/#{Animal.first.id}") } 
+    during { 
+      @view.to_s
+    }.behold! { 
+      @date_source.should_receive(:current_date_as_string).and_return('2012-01-03')
+    }
+    assert { @result.include? delete_button("animal/#{Animal.first.id}?as_of=2012-01-03") } 
   end
 end
 
--- view/animal-list-view.rb
+++ view/animal-list-view.rb
@@ -2,6 +2,7 @@ require 'view/util'
 
 class AnimalListView < Erector::Widget
   include ViewHelper
+  needs :animals, :date_source
 
   def content
     html do 
@@ -15,7 +16,8 @@ class AnimalListView < Erector::Widget
           animals_sorted_by_name.each do | a |
             tr do 
               td { text a.name }
-              td { rawtext delete_button("animal/#{a.id}") }
+              today = @date_source.current_date_as_string
+              td { rawtext delete_button("animal/#{a.id}?as_of=#{today}") }
             end
           end
         end
