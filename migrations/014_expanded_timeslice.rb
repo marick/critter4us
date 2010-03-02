28a76b001c41f7bc88c490fd4f1b6df2987fec18
Brian Marick <marick@exampler.com>
2010-03-02 09:30:08 -0600

    Migration makes new timeslices and cached exclusions.

---
+++ migrations/014_expanded_timeslice.rb
@@ -0,0 +1,89 @@
+require 'util/constants'
+require 'model/requires'
+require 'pp'
+
+class ExpandedTimeslice014 < Sequel::Migration
+  def up
+    puts "==== deleting left-over column"
+    DB.drop_column :reservations, :morning
+
+    puts "==== adding bit(3) field for TimeSet and separate start/end dates (for code migration)"
+    DB.add_column :reservations, :first_date, Date
+    DB.add_column :reservations, :last_date, Date
+    DB.add_column(:reservations, :time_set, :bit, :size => 3, :null => false, 
+                  :default => "000")
+
+
+    puts "==== copying values in new form"
+    DB[:reservations].filter(:time => MORNING).update(:time_set => "100")
+    DB[:reservations].filter(:time => AFTERNOON).update(:time_set => "010")
+    DB[:reservations].filter(:time => EVENING).update(:time_set => "001")
+
+    DB[:reservations].update(:first_date => :date)
+    DB[:reservations].update(:last_date => :date)
+
+    puts "==== making new tables"
+    # DB.create_table :timeslices do
+    #   primary_key :id
+    #   Date :first_date, :null => false
+    #   Date :last_date, :null => false
+    #   bit :time_set, :size => 3, :null => false
+    # end
+
+    DB.create_table :excluded_because_in_use do 
+      primary_key :id
+      Date :first_date, :null => false
+      Date :last_date, :null => false
+      bit :time_set, :size => 3, :null => false
+      foreign_key :reservation_id, :reservations
+      foreign_key :animal_id, :animals
+    end
+
+    DB.create_table :excluded_because_of_blackout_period do
+      primary_key :id
+      Date :first_date, :null => false
+      Date :last_date, :null => false
+      bit :time_set, :size => 3, :null => false
+      foreign_key :reservation_id, :reservations
+      foreign_key :animal_id, :animals
+      foreign_key :procedure_id, :procedures
+    end
+
+    # ==== filling new tables
+
+    Reservation.each do | reservation |
+      reservation.animals.each do | animal | 
+        DB[:excluded_because_in_use].insert(:first_date => reservation.values[:first_date],
+                                            :last_date => reservation.values[:last_date],
+                                            :time_set => reservation.values[:time_set],
+                                            :reservation_id => reservation.values[:id],
+                                            :animal_id => animal.id)
+      end
+      reservation.uses.each do | use | 
+        animal = Animal[use.animal_id]
+        procedure = Procedure[use.procedure_id]
+        
+        next if procedure.days_delay == 0
+
+        dataset = DB[:excluded_because_of_blackout_period]
+
+        dataset.insert(:first_date => reservation.values[:first_date] - procedure.days_delay + 1,
+                       :last_date => reservation.values[:last_date] + procedure.days_delay - 1,
+                       :time_set => reservation.values[:time_set],
+                       :reservation_id => reservation.values[:id],
+                       :animal_id => animal.id,
+                       :procedure_id => procedure.id)
+      end
+    end
+  end
+
+  def down
+    DB.drop_column :reservations, :time_set
+    DB.drop_column :reservations, :first_date
+    DB.drop_column :reservations, :last_date
+    # DB.drop_table :timeslices
+    DB.drop_table :excluded_because_in_use
+    DB.drop_table :excluded_because_of_blackout_period
+    DB.add_column :reservations, :morning, :boolean # dummy so it can be deleted.
+  end
+end
