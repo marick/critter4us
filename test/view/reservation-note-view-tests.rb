847de2b097fb24d60c70fc49b96ffe0295d24e45
Brian Marick <marick@exampler.com>
2011-10-07 17:34:49 -0500

    A rudimentary notes view

---
+++ test/view/reservation-note-view-tests.rb
@@ -0,0 +1,21 @@
+require './test/testutil/requires'
+require './model/requires'
+require './view/requires'
+
+class ReservationNoteViewTests < FreshDatabaseTestCase
+  include ViewHelper
+  include HtmlAssertions
+
+  should "contain a form to update notes" do
+    note_content = "THIS IS A NOTE"
+    reservation = Reservation.random(:note => note_content)
+    output = ReservationNoteView.new(:reservation => reservation).to_html
+    assert_text_has_attributes(output, 'form', :method=>"POST",
+                               :action => "/2/reservation/#{reservation.id}/note")
+    assert_text_has_attributes(output, 'input',
+                                :type=>"submit",  :value => 'Update Note')
+    assert_text_has_attributes(output, 'textarea', :name => "note")
+    assert_text_has_selector(output, 'textarea', :text => note_content)
+  end
+end
+
--- view/reservation-note-view.rb
+++ view/reservation-note-view.rb
@@ -2,11 +2,24 @@ require './view/util'
 
 class ReservationNoteView < Erector::Widget     
   include ViewHelper
+  include ReservationHelper
 
   def content
     html do 
       head do
         title 'Reservation Note'
+      end
+      body do 
+        p { long_form(@reservation) }
+        form(:method => "POST",
+             :action => "/2/reservation/#{@reservation.id}/note") do
+          textarea(:name => "note") do
+            rawtext @reservation.note
+          end
+          p do 
+            input(:type => 'submit', :value=>"Update Note")
+          end
+        end
       end
     end
   end
