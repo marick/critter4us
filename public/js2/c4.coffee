aed54df2285fccab536d65f60ae0fb311bd4aa32
Brian Marick <marick@exampler.com>
2011-10-18 17:33:40 -0500

    This version works.

--- public/js2/c4.coffee
+++ public/js2/c4.coffee
@@ -2,29 +2,29 @@ global = (exports ? this)
 
 class global.OnStarts
 
-  @hide_empty_textile_div: -> 
-    $('.textile').filter (index) ->
-      this.innerHTML.match /^\s*$/
-    .hide("slow")
-    $('.textile').filter (index) ->
-      !this.innerHTML.match(/^\s*$/)
-    .show("slow")
+  @isEmpty: (tag) ->
+    tag.innerHTML.match /^\s*$/
+    
+
+  @hide_empty_textile_div: (duration) -> 
+    $('.textile').filter ->
+      OnStarts.isEmpty(this)
+    .slideUp(duration)
+    $('.textile').filter ->
+      !OnStarts.isEmpty(this)
+    .slideDown(duration)
 
 
   @update_textile_div: (newStuff) ->
     $('.textile').html(newStuff)
 
   @get_note_editing_page: (reservation_id) ->
-    OnStarts.hide_empty_textile_div()
+    OnStarts.hide_empty_textile_div(0)
     $('body').attr('onbeforeunload', "")
-    $('#note_form').ajaxForm({target: '.textile'}, ->
-               OnStarts.hide_empty_textile_div())
+    $('#note_form').ajaxForm {
+                       target: '.textile'
+                       success: (responseText) ->
+                          OnStarts.hide_empty_textile_div("fast")}
 
 
- # @get_note_editing_page: (reservation_id) ->
- #   $('input#update_note').click -> 
- #     $.post "/2/reservation/#{reservation_id}/note",
- #            $('form#note_form').serialize(),
- #            -> $('#note').load("/2/reservation/#{reservation_id}/notetext")
- #   $('#note').load("/2/reservation/#{reservation_id}/notetext")
 
--- public/js2/c4.js
+++ public/js2/c4.js
@@ -3,24 +3,28 @@
   global = typeof exports !== "undefined" && exports !== null ? exports : this;
   global.OnStarts = (function() {
     function OnStarts() {}
-    OnStarts.hide_empty_textile_div = function() {
-      $('.textile').filter(function(index) {
-        return this.innerHTML.match(/^\s*$/);
-      }).hide("slow");
-      return $('.textile').filter(function(index) {
-        return !this.innerHTML.match(/^\s*$/);
-      }).show("slow");
+    OnStarts.isEmpty = function(tag) {
+      return tag.innerHTML.match(/^\s*$/);
+    };
+    OnStarts.hide_empty_textile_div = function(duration) {
+      $('.textile').filter(function() {
+        return OnStarts.isEmpty(this);
+      }).slideUp(duration);
+      return $('.textile').filter(function() {
+        return !OnStarts.isEmpty(this);
+      }).slideDown(duration);
     };
     OnStarts.update_textile_div = function(newStuff) {
       return $('.textile').html(newStuff);
     };
     OnStarts.get_note_editing_page = function(reservation_id) {
-      OnStarts.hide_empty_textile_div();
+      OnStarts.hide_empty_textile_div(0);
       $('body').attr('onbeforeunload', "");
       return $('#note_form').ajaxForm({
-        target: '.textile'
-      }, function() {
-        return OnStarts.hide_empty_textile_div();
+        target: '.textile',
+        success: function(responseText) {
+          return OnStarts.hide_empty_textile_div("fast");
+        }
       });
     };
     return OnStarts;
