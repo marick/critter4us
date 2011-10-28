d50599d4ab031f9738642554542d531f2c7a82cf
Brian Marick <marick@exampler.com>
2011-10-27 19:27:58 -0500

    Extracted common method

--- src/db/functional_timeslice.rb
+++ src/db/functional_timeslice.rb
@@ -5,14 +5,19 @@ class FunctionalTimeslice < FunctionalHash
 
   def self.from_browser(raw_data)
     data = JSON.parse(Base64.decode64(raw_data))
-    F(:first_date => Date.parse(data['first_date']),
-      :last_date => Date.parse(data['last_date']),
-      :time_bits => TimeSet.new(data['times']).bits)
+    from_time_data(Date.parse(data['first_date']), Date.parse(data['last_date']),
+                   TimeSet.new(data['times']).bits)
   end
 
   def self.from_reservation(reservation)
-    F(:first_date => reservation.data.first_date,
-      :last_date => reservation.data.last_date,
-      :time_bits => reservation.data.time_bits)
+    from_time_data(reservation.data.first_date, reservation.data.last_date,
+                   reservation.data.time_bits)
   end
+
+  def self.from_time_data(first_date, last_date, time_bits)
+    F(:first_date => first_date,
+      :last_date => last_date,
+      :time_bits => time_bits)
+  end
+
 end
