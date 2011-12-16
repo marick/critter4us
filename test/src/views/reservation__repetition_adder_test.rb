255aadd7b450c3ab16f16a16779a45dcc6253a61
Brian Marick <marick@exampler.com>
2011-12-16 11:33:06 -0600

    More complete HAML test

--- test/src/views/reservation__repetition_adder_test.rb
+++ test/src/views/reservation__repetition_adder_test.rb
@@ -3,14 +3,29 @@ require './strangled-src/model/requires'
 require './src/views/requires'
 
 class GetRepetitionAddingPageTest < ViewTestCase
+  include HtmlAssertions
 
-  def render(hash)
-    reservation = Reservation.random(hash)
-    @renderer.render_page(:reservation__repetition_adder, :reservation => reservation)
+  def setup
+    super
+    reservation = Reservation.random(instructor: "Dr. Dawn")
+    @html = @controller.haml(:reservation__repetition_adder,
+                             :locals => {
+                               start_date: Date.new(2001, 2, 3),
+                               reservation: reservation,
+                               rest_links: [Href.link("uri", "fulfillment")]
+                             })
   end
 
-  should "use reservation to populate page upon GET" do
-    assert { render(:instructor => 'Dr. Dawn')['Dr. Dawn'] }
+  should "include some information about the reservation" do
+    assert { @html['Dr. Dawn'] }
+  end
+
+  should "include a Javascript starting date" do 
+    assert { @html['new Date(2001, 1, 3)'] }
+  end
+
+  should "include the fulfillment link" do
+    assert_text_has_attributes(@html, "link", href: "uri", rel: "fulfillment")
   end
 
 end
