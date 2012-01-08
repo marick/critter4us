fa18e8a1aac06a9cf2156809cd1117cc5ba50908
Brian Marick <marick@exampler.com>
2012-01-08 09:52:52 -0600

    Silly synonyms

---
+++ src/util/logging.rb
@@ -0,0 +1,6 @@
+# def Logger.tmi(*args); Logger.verbose(*args); end
+def Logger.cya(*args); Logger.debug(*args); end
+def Logger.fyi(*args); Logger.info(*args); end
+def Logger.wtf(*args); Logger.warn(*args); end
+def Logger.omg(*args); Logger.error(*args); end
+def Logger.fml(*args); Logger.fatal(*args); end
