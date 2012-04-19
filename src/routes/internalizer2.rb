fddba7c39eadfab5a49d6da34d59789bebfd1b83
Brian Marick <marick@exampler.com>
2012-04-19 15:11:33 -0500

    Conversion to a duration of past days

--- src/routes/internalizer2.rb
+++ src/routes/internalizer2.rb
@@ -3,7 +3,14 @@ require 'base64'
 require 'json'
 
 class Internalizer2
-  def initialize(strangled)
+  def initialize(strangled = nil)
     @strangled = strangled
   end
+
+  def days_in_the_past(day_string)
+    days = day_string.to_i
+    days = 3650 if days == 0
+    days
+  end
+
 end
--- test/src/routes/internalizer2_test.rb
+++ test/src/routes/internalizer2_test.rb
@@ -2,4 +2,25 @@ require './test/testutil/requires'
 require './src/routes/internalizer2'
 
 class Internalizer2Tests < Test::Unit::TestCase
+
+  def setup
+    @subject = Internalizer2.new
+    @before_app = Date.new(2007, 1, 1)
+  end
+
+  context "#past_date" do
+    should "convert strings to numbers" do 
+      assert { @subject.days_in_the_past("1") == 1 } 
+    end
+
+    should "treat words as 'long ago' (before app existed)" do
+      actual = @subject.days_in_the_past("bazillion")
+      assert { Date.today - actual < @before_app }
+    end
+
+    should "also treat the empty string as 'long ago'" do 
+      actual = @subject.days_in_the_past("")
+      assert { Date.today - actual < @before_app }
+    end
+  end
 end
